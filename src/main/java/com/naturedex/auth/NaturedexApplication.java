6f28a6b54ecb1538ee712bca97f278d1f2bb45cb
Sebastian <sebastian.milczarek12@gmail.com>
2025-07-06 12:05:40 +0200

    project initialization

---
+++ src/main/java/com/naturedex/auth/NaturedexApplication.java
@@ -0,0 +1,13 @@
+package com.naturedex.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturedexApplication.class, args);
+	}
+
+}
